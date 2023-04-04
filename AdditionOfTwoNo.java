975ea205bd6ac862995c80200ae32de95219fea7
Atul-tiwari2810 <atul@wowfinstack.com>
2023-04-04 11:51:02 +0100

    Addition class addad

---
+++ AdditionOfTwoNo.java
@@ -0,0 +1,8 @@
+public class AdditionOfTwoNo {
+    public static void main(String []args){
+        int a=10;
+        int b=20;
+        int c=30;
+        system.out.println("addition of two no is:-"+c);
+    }
+}
